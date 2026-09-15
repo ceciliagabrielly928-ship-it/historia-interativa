86654e93dea0f382b3adec8d607aa198f20c9907
ceciliagabrielly928-ship-it <ceciliagabrielly928@gmail.com>
2026-09-15 20:05:39 +0000

    v7

--- story.py
+++ story.py
@@ -7,7 +7,7 @@ HISTORY = {
     # ==========================================
     "inicio": {
         "tipo": "inicio",
-        "imagem": "assets/images/capa.png",
+        "imagem": "assets/imagens/capa.png",
         "titulo": "A Garrafa do Futuro",
         "descricao": "Uma história sobre escolhas e o destino do planeta"
     },
